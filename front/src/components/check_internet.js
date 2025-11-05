bd1eb5b4b8d52688f0e2a8e01a6f1541746fbbbf
Mateusz Pogoda <126457939+MatheWeather2137@users.noreply.github.com>
2025-11-05 15:18:52 +0100

    asa

--- front/src/components/check_internet.js
+++ front/src/components/check_internet.js
@@ -3,12 +3,20 @@ import Button from "@mui/material/Button";
 import Toggle_Button from "./toggle_button";
 import Slider from "@mui/material/Slider";
 import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 export default function Check_internet() {
   const [alignment, setAlignment] = useState("google.com");
   const [internet, setInternet] = useState(true);
   const [color, setColor] = useState("");
   const [packages, setPackages] = useState(1);
+  const [time, setTime] = useState([]);
 
   const check = async () => {
     try {
@@ -43,11 +51,30 @@ export default function Check_internet() {
       <Box sx={{ width: 300 }}>
         <Slider
           defaultValue={1}
+          min={1}
           max={4}
           aria-label="Default"
           valueLabelDisplay="auto"
           onChange={(e) => setPackages(e.target.value)}
         />
+      </Box>
+      <Box sx={{ width: 500 }}>
+        <TableContainer component={Paper}>
+          <Table sx={{ width: 400 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Czas</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </div>
   );
